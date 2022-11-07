650370d267daf290e9bffd27a925ee7e725103ac
Prathap1699 <pradhap1699@gmail.com>
2022-11-07 15:59:33 +0530

    task2

---
+++ src/org/cts/Details.java
@@ -0,0 +1,53 @@
+package org.cts;
+
+import java.util.Scanner;
+
+public class Details {
+
+	public static void main(String[] args) {
+		
+		Scanner s=new Scanner(System.in);
+		
+		System.out.println("enter the name ");
+		String string = s.next();
+		System.out.println("name is;"+string);
+		
+		String string2 = s.nextLine();
+		System.out.println(string2);
+		
+		
+		
+		
+		
+
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
